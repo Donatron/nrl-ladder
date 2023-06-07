b81ddb1a78fdae0e47b3451c720d06a7bced69bf
donmacarthur <don@bluemeg.com>
2023-06-07 11:39:34 +0700

    api-integration: add actions and reducers to call api, update adder data to read from returned state, add ladder type selector

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,39 @@
 import React, { useEffect, useState } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
-import { getOfficialLadder } from './actions'
+import { getOfficialLadder,setLoading } from './actions'
 
 import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
 import Ladder from './components/Ladder/Ladder'
 import LadderType from './components/LadderType/LadderType'
 import Layout from './components/Layout/Layout'
+import Loading from './components/Loading/Loading'
 
 import './app.css'
 
 const App = () => {
   const dispatch = useDispatch()
   const [ladderType, setLadderType] = useState('standard')
+  const { isLoading } = useSelector(state => state.loading)
 
   useEffect(() => {
+    dispatch(setLoading(true))
     dispatch(getOfficialLadder())
   }, [])
+  
 
   return (
     <Layout>
       <Header />
-      <LadderType setLadderType={setLadderType} />
-      <Ladder ladderType={ladderType}/>
+      {
+        !isLoading ? (
+          <>
+            <LadderType setLadderType={setLadderType} />
+            <Ladder ladderType={ladderType}/>
+          </>
+          ) : <Loading />
+      }
       <Footer />
     </Layout>
   )
--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,7 +1,66 @@
-export const SET_LOADING = 'SET_LOADING'
-export const SET_ERRORS = 'SET_ERRORS'
+import axios from 'axios'
+
+export const GET_NO_BYE_POINTS_SUCCESS = 'GET_NO_BYE_POINTS_SUCCESS'
 export const GET_OFFICIAL_LADDER = 'GET_OFFICIAL_LADDER'
+export const GET_OFFICIAL_LADDER_SUCCESS = 'GET_OFFICIAL_LADDER_SUCCESS'
+export const GET_WIN_PERCENTAGE= 'GET_WIN_PERCENTAGE'
+export const SET_ERRORS = 'SET_ERRORS'
+export const SET_LOADING = 'SET_LOADING'
 
 export const getOfficialLadder = () => async dispatch => {
-  // This is where I call the api
+  let apiUrl
+  if(process.env.NODE_ENV === 'development') {
+    apiUrl = 'http://localhost:8080'
+  } else {
+    apiUrl = 'yettobedetermined'
+  }
+
+  const response = await axios.get(apiUrl)
+
+  dispatch({
+    type: GET_OFFICIAL_LADDER_SUCCESS,
+    payload: response.data
+  })
+  dispatch(setLoading(false))
+  dispatch(getNoByePoints(response.data.data))
+  dispatch(getPercentageWins(response.data.data))
+}
+
+const getNoByePoints = (data) => dispatch => {
+  const noyByePoints = data.map((el) => ({
+    ...el,
+    points: el.points - (el.byes * 2)
+  }))
+    .sort((a, b) => {
+      return (parseInt(a.points) > parseInt(b.points))
+        && (parseInt(a.pointsDiff) < parseInt(b.pointsDiff)) ? -1 : 1
+    })
+
+  dispatch({
+    type: GET_NO_BYE_POINTS_SUCCESS,
+    payload: noyByePoints
+  })
+}
+
+const getPercentageWins = (data) => dispatch => {
+  const percentageWins = data.map((el) => ({
+    ...el,
+    winPercentage: ((el.wins / el.gamesPlayed) * 100).toFixed(2)
+  }))
+    .sort((a, b) => {
+      return (a.winPercentage > b.winPercentage)
+        && parseInt(a.pointsDiff) < parseInt(b.pointsDiff) ? -1 : 1
+    })
+
+  dispatch({
+    type: GET_WIN_PERCENTAGE,
+    payload: percentageWins
+  })
+}
+
+export const setLoading = (isLoading) => dispatch => {
+  return dispatch({
+    type: SET_LOADING,
+    payload: isLoading
+  })
 }
