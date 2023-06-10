b52158f641f066a0f0a9e95ba455cded3e3481f3
donmacarthur <don@bluemeg.com>
2023-06-10 09:46:15 +0700

    split-api-and-client: move api code to separate repo, move client code to top level direcroty

--- api/routers/webScrapingRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-
-const router = express.Router();
-
-const WebScrapingController = require('../contollers/WebScrapingController')
-
-router.get('/', WebScrapingController.getOfficialLadder)
-
-module.exports = router
